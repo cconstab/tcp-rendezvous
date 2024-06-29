132159b666b0626f82c59063b9f3caae60777954
Colin Constable <colin@constable.cc>
2024-06-29 16:35:34 -0700

    fix: empyty

--- lib/src/socket_connector.dart
+++ lib/src/socket_connector.dart
@@ -159,7 +159,7 @@ class SocketConnector {
           // //await Future.delayed(const Duration(milliseconds: 10));
           bool empty = false;
           while (empty == false) {
-            empty = side.farSide!.buffer.isNotEmpty;
+            empty = side.farSide!.buffer.isEmpty;
             await Future.delayed(const Duration(milliseconds: 10));
           }
           side.farSide!.sink.add(data);
