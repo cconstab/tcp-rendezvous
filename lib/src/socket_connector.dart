81dacf882866fdda6703b64616ca43ba68dbc13d
Colin Constable <colin@constable.cc>
2024-07-01 11:07:32 -0700

    Update socket_connector.dart

--- lib/src/socket_connector.dart
+++ lib/src/socket_connector.dart
@@ -172,11 +172,13 @@ class SocketConnector {
           //}
           
           side.farSide!.sink.add(data);
-        }, onDone: () {
+        }, onDone: () async {
           _log('stream.onDone on side ${side.name}');
+          await Future.delayed(const Duration(seconds: 10));
           _destroySide(side);
-        }, onError: (error) {
+        }, onError: (error) async {
           _log('stream.onError on side ${side.name}: $error', force: true);
+            await Future.delayed(const Duration(seconds: 10));
           _destroySide(side);
         });
       }
